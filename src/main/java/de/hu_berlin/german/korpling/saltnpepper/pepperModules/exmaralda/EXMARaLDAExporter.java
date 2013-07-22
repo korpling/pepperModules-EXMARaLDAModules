91beba95caa93bd4fc1bbdc2be647109abcf653b
FlorianZipser <f.zipser@gmx.de>
2013-07-22 15:16:37 +0000

    removed some uncommented stuff
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/PepperModules/EXMARaLDAModules/trunk@8244 47085195-c50e-4317-8f43-7cb808db0f00

--- src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/exmaralda/EXMARaLDAExporter.java
+++ src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/exmaralda/EXMARaLDAExporter.java
@@ -69,48 +69,4 @@ public class EXMARaLDAExporter extends PepperExporterImpl implements PepperExpor
 			}
 		}
 	}
-	
-//	@Override
-//	public void start(SElementId sElementId) throws PepperModuleException 
-//	{
-//		if (sElementId.getSIdentifiableElement() instanceof SDocument)
-//		{
-//			SDocument sDoc= (SDocument) sElementId.getSIdentifiableElement();
-//			BasicTranscription basicTranscription= ExmaraldaBasicFactory.eINSTANCE.createBasicTranscription();
-//			
-//			// start: mapping
-//				Salt2EXMARaLDAMapper mapper= new Salt2EXMARaLDAMapper();
-//				try
-//				{
-//					mapper.map2BasicTranscription(sDoc, basicTranscription);
-//				}catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			// start: mapping
-//			
-//			this.createFolderStructure(sElementId);
-//			//create uri to save
-//			URI uri= URI.createFileURI(this.getCorpusDefinition().getCorpusPath().toFileString()+ "/" + sElementId.getSElementPath().toFileString()+ "." + FILE_EXTENION);
-//			try {
-//				this.saveToFile(uri, basicTranscription);
-//			} catch (IOException e) {
-//				throw new EXMARaLDAExporterException("Cannot write document with id: '"+sElementId.getSElementPath().lastSegment()+"' into uri: '"+uri+"'.", e);
-//			}
-//		}
-//	}
-	
-//	private void saveToFile(URI uri, BasicTranscription basicTranscription) throws IOException
-//	{
-//		// create resource set and resource 
-//		ResourceSet resourceSet = new ResourceSetImpl();
-//		// Register XML resource factory
-//		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("exb",new EXBResourceFactory());
-//		//load resource 
-//		Resource resource = resourceSet.createResource(uri);
-//		if (resource== null)
-//			throw new EXMARaLDAExporterException("Cannot save a resource to uri '"+uri+"', because the given resource is null.");
-//		
-//		resource.getContents().add(basicTranscription);
-//		resource.save(null);
-//	}
 }
